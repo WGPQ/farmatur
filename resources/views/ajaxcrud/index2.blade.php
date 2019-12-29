d63623587995b58ff45228b05ba1f935ba9d002d
WGPQ <wgpumaq@utn.edu.ec>
2019-12-29 05:46:34 -0500

    Esto es lo que cambie

--- resources/views/ajaxcrud/index2.blade.php
+++ resources/views/ajaxcrud/index2.blade.php
@@ -7,6 +7,6 @@
     <title>holse2</title>
 </head>
 <body>
-    
+    <h5>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, aperiam? Aliquid obcaecati distinctio corporis rerum ullam eveniet at, quisquam odio non dolore maxime, porro, labore placeat perferendis consequatur doloribus accusamus?</h5>
 </body>
 </html>
